268cd50e1f646d73182e0abe5b7fcf99e0e1fdcd
Son <Son@SonPHSE61822>
2016-04-12 21:14:42 -0700

    testing commit

---
+++ test/Scholarship.java
@@ -0,0 +1,30 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Son
+ */
+class Scholarship {
+
+    static int getAward(double gpa) {
+        if (gpa > 4 || gpa <0) {
+            return -1;
+        } else if (gpa >= 3.8) {
+            return 100;
+        } else if (gpa >= 3.6) {
+            return 80;
+        } else if (gpa >= 3.4) {
+            return 70;
+        } else {
+            return 0;
+        }
+
+    }
+
+}
